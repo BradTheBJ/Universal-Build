c41aa1a67c5adea3af5c1880d34088f7ba6d7fb5
Brad Ben Joya <bradbenjoya6@gmail.com>
2026-02-06 23:38:38 +0200

    Improved code consitency

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -4,52 +4,49 @@
 #include <fstream>
 #include <iostream>
 
-
 std::vector<Token> lexResult;
 
 void lex() {
-  std::ifstream file("build.ub");
-  if (file.fail()) {
-    std::cout << "\033[31m"
-              << std::format("Could not open build.ub for reading\n")
-              << "\033[0m";
-    std::exit(1);
-  }
+	std::ifstream file("build.ub");
+	if (file.fail()) {
+		std::cout << "\033[31m" << std::format("Could not open build.ub for reading\n") << "\033[0m";
+		std::exit(1);
+	}
 
-  std::string buffer;
-  char c;
-  while (file.get(c)) {
-    if (buffer.empty() && std::isspace(c)) {
-      continue;
-    }
+	std::string buffer;
+	char c;
+	while (file.get(c)) {
+		if (buffer.empty() && std::isspace(c)) {
+			continue;
+		}
 
-    if (c == '#') {
-      while (file.get(c) && c != '\n') {
-      }
-      buffer.clear();
-      continue;
-    }
+		if (c == '#') {
+			while (file.get(c) && c != '\n') {
+			}
+			buffer.clear();
+			continue;
+		}
 
-    buffer += c;
+		buffer += c;
 
-    if (c == ')') {
-      Token token;
-      if (buffer.starts_with("setCompiler")) {
-        token.type = TokenType::SET_COMPILER;
-      } else if (buffer.starts_with("addFlag")) {
-        token.type = TokenType::ADD_FLAG;
-      } else if (buffer.starts_with("exec")) {
-        token.type = TokenType::EXEC;
-      } else if (buffer.starts_with("detectCompiler")) {
-        token.type = TokenType::DETECT_COMPILER;
-      } else {
-        buffer.clear();
-        continue;
-      }
+		if (c == ')') {
+			Token token;
+			if (buffer.starts_with("setCompiler")) {
+				token.type = TokenType::SET_COMPILER;
+			} else if (buffer.starts_with("addFlag")) {
+				token.type = TokenType::ADD_FLAG;
+			} else if (buffer.starts_with("exec")) {
+				token.type = TokenType::EXEC;
+			} else if (buffer.starts_with("detectCompiler")) {
+				token.type = TokenType::DETECT_COMPILER;
+			} else {
+				buffer.clear();
+				continue;
+			}
 
-      token.value = buffer;
-      lexResult.push_back(token);
-      buffer.clear();
-    }
-  }
+			token.value = buffer;
+			lexResult.push_back(token);
+			buffer.clear();
+		}
+	}
 }
